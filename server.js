2d3b8fd692a8ff33b968db340af9975d1bbdd6e9
Rushit Patel <patelrushit99@gmail.com>
2016-10-15 04:53:06 +0000

    added middlewere of morgon and modyparser

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var morgan = require("morgan");
-
+var config = require("./config");
 
 var app = express();
 
-app.listen(process.env.PORT,process.env.IP,function(err){
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+app.listen(config.port,process.env.IP,function(err){
     if(err){
         console.log(err);
     }else{
-        console.log("listing on port " + process.env.PORT + "and IP : " + process.env.IP);
+        console.log("listing on port "+config.port + process.env.PORT + "and IP : " + process.env.IP);
         
     }
+});
+
+app.get('*', function (req, res) {
+  res.sendFile(__dirname + '/public/views/index.html');
 });
 app.get('/', function (req, res) {
   res.send('Hello World!');
